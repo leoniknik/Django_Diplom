3a11670173c85b173f5a4ad6b347c5d48de00277
leoniknik <leoniknik@mail.ru>
2017-04-15 22:45:40 +0300

    add some html

--- Diplom/models.py
+++ Diplom/models.py
@@ -5,11 +5,10 @@ from django.contrib.auth.base_user import AbstractBaseUser
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, firstname, lastname, phone):
-        if email and password and firstname and lastname and phone:
-            user = self.model(email=self.normalize_email(email), firstname=firstname, lastname=lastname, phone=phone)
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
+        user = self.model(email=self.normalize_email(email), firstname=firstname, lastname=lastname, phone=phone)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def update_user(self, user_id, email, password, firstname, lastname, phone):
         user = User.objects.get(pk=user_id)
--- Diplom/views.py
+++ Diplom/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from Diplom.models import User
+from Diplom.authorization import Auth
 
-# Create your views here.
+
+def home(request):
+    try:
+        return render(request, 'main.html')
+    except Exception as e:
+        print(e)
+        return render(request, 'main.html')
+
+
+def signin(request):
+    try:
+        if request.method == 'GET':
+            return render(request, 'signin.html')
+        elif request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+            user = Auth().authenticate(email, password)
+            if user is not None:
+                return render(request, "temp.html")
+            else:
+                return render(request, 'signin.html')
+    except Exception as e:
+        print(e)
+        return render(request, 'signin.html')
+
+
+def signup(request):
+    try:
+        if request.method == 'GET':
+            return render(request, 'signup.html')
+        elif request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+            repeat_password = request.POST['repeat_password']
+            firstname = request.POST['firstname']
+            lastname = request.POST['lastname']
+            phone = request.POST['phone']
+            if password != repeat_password:
+                return render(request, 'signup.html')
+            else:
+                User.objects.create_user(email, password, firstname, lastname, phone)
+                return render(request, 'signin.html')
+    except Exception as e:
+        print(e)
+        return render(request, 'signup.html')
